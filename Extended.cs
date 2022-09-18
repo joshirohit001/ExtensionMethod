2a4d63fd5fd3882234e40eeb2a034d0548251614
Rohit Joshi <rj.rohitjoshi@hotmail.com>
2022-09-18 23:11:23 +0530

    Advanced Extension method

--- Extended.cs
+++ Extended.cs
@@ -1,16 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ExtensionMethod
+﻿namespace ExtensionMethod
 {
     internal static class Extended
     {
         public static int SquareOfNumber(this int number)
         {
             int result = number * number;
+            return result;
+        }
+        public static int ListIndexerMultiply(this List<int> objInt)
+        {
+           int result = objInt[0] * objInt[1];
             return result;
         }
     }
--- Program.cs
+++ Program.cs
@@ -3,6 +3,13 @@
 using ExtensionMethod;
 
 int number = 10;
-int result = number.SquareOfNumber();
-Console.WriteLine(result);
+int sqOfNumber = number.SquareOfNumber();
+/*------------------------------------------------*/
+/*------------------------------------------------*/
+List<int> obj = new List<int>();
+obj.Add(10);
+obj.Add(20);
+int Multiply = obj.ListIndexerMultiply();
+Console.WriteLine(sqOfNumber);
+Console.WriteLine(Multiply);
 Console.ReadLine();

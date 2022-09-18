694d85b7935fd6fafcc9d8f2ba768b347b0b6858
Dell <Dell@DESKTOP-T8VHU5N>
2022-09-18 22:53:47 +0530

    Add project files.

---
+++ Extended.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethod
+{
+    internal static class Extended
+    {
+        public static int SquareOfNumber(this int number)
+        {
+            int result = number * number;
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ExtensionMethod;
+
+int number = 10;
+int result = number.SquareOfNumber();
+Console.WriteLine(result);
+Console.ReadLine();
